e7d879a3bd431f98f709bf0ef4809839d2ed5a80
George Garcia <jorge.90gap@gmail.com>
2021-12-07 12:28:12 +0900

    Agenda Delete controller and view

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -6,4 +6,16 @@ class AssignMailer < ApplicationMailer
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
   end
+
+  def change_owner_mail(team)
+    @email = team.owner.email
+    @team = team
+    mail to: @email, subject: I18n.t('views.messages.delegated_owner')
+  end
+
+  def delete_agenda_mail(agenda)
+    @email = agenda.team.members.map(&:email)
+    @agenda = agenda
+    mail to: @email, subject: I18n.t('views.messages.delete_agenda')
+  end
 end
